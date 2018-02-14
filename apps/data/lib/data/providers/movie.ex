245f1e478e21ba6565f1da88028fa99e2e4d4386
Jeremy Tregunna <jeremy@tregunna.ca>
2018-02-13 18:29:37 -0600

    add functions to grab records for movies

--- apps/data/lib/data/providers/movie.ex
+++ apps/data/lib/data/providers/movie.ex
@@ -1,2 +1,19 @@
 defmodule Data.Providers.Movie do
+  alias Data.Cassandra
+
+  @keyspace "ea28b544e93cff97e42b770e"
+  @table_name "movie_list"
+
+  def get(movie_id: movie_id) do
+    statement = "SELECT movie_id, movie_title, popularity, ingest_date, ingested_info FROM #{@keyspace}.#{@table_name} WHERE movie_id = ? LIMIT 1;"
+    values    = [
+      {"text", movie_id}
+    ]
+    Cassandra.execute(statement, values)
+  end
+
+  def all() do
+    statement = "SELECT movie_id, movie_title, popularity, ingest_date, ingested_info FROM #{@keyspace}.#{@table_name};"
+    Cassandra.execute(statement, [])
+  end
 end
