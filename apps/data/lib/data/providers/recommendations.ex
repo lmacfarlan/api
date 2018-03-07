201745d03a339230d2edd6a722aafc8bf6f0bc1f
Wesley Pannell <wes@lemonsrc.io>
2018-03-07 10:13:15 -0600

    Add recommendations endpoint

---
+++ apps/data/lib/data/providers/recommendations.ex
@@ -0,0 +1,43 @@
+defmodule Data.Providers.Recommendations do
+  @moduledoc false
+
+  require Logger
+
+  alias Data.Cassandra
+
+  @default_page_size 100
+  @keyspace "ea28b544e93cff97e42b770e"
+  @table_name "recommendations"
+
+  @doc """
+  Get all Recommendations
+
+  ## Options
+
+  This function accepts the following options:
+
+    * `page_size` - (integer) number of results per page to return
+    * `paging_state` - (binary) cursor for finding the next page of results
+
+  """
+  def all(opts \\ []) do
+    query =
+      select_all_query()
+    params =
+      []
+    opts =
+      default_opts()
+      |> Keyword.merge(opts)
+      # Filter out keywords with nil value
+      |> Enum.filter(&!!elem(&1, 1))
+    Cassandra.execute(query, params, opts)
+  end
+
+
+  # Private
+
+
+  defp default_opts, do: [page_size: @default_page_size]
+
+  defp select_all_query, do: "SELECT * FROM #{@keyspace}.#{@table_name}"
+end
